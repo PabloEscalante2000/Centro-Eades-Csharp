a5e0c55a4735b32800ba40def77a45fe552aeeb9
rhodas agapito <43115211+rhodas1980@users.noreply.github.com>
2022-10-21 14:21:01 -0500

    Conexion_ADO
    ConnectionStrings["Citas"]

---
+++ CentroEades_ADO/ConexionADO.cs
@@ -0,0 +1,26 @@
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CentroEades_ADO
+{
+    internal class ConexionADO
+    {
+        public string GetCnx()
+        {
+            string strCnx =
+                ConfigurationManager.ConnectionStrings["Citas"].ConnectionString;
+            if (object.ReferenceEquals(strCnx, string.Empty))
+            {
+                return string.Empty;
+            }
+            else
+            {
+                return strCnx;
+            }
+        }
+    }
+}
